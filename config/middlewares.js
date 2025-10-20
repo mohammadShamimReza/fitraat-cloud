dd5c58349724dd45287a651a7c5cb79b88a73023
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2025-10-20 15:05:20 +0600

    added 2 more link for cors error

--- config/middlewares.js
+++ config/middlewares.js
@@ -22,6 +22,7 @@ module.exports = [
             "'self'",
             "https://www.fitraat.com",
             "http://localhost:3000",
+            "https://detox-frontend-web.vercel.app/",
           ],
         },
       },
@@ -34,6 +35,7 @@ module.exports = [
       origin: [
         "https://www.fitraat.com", // ✅ production frontend
         "http://localhost:3000", // ✅ local dev
+        "https://detox-frontend-web.vercel.app/",
       ],
       headers: ["*"],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
