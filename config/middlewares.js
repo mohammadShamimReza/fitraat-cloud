b14292c4aaf890451da711499ffb0b77508ce30e
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2025-10-21 22:46:32 +0600

    remove / from link

--- config/middlewares.js
+++ config/middlewares.js
@@ -22,7 +22,7 @@ module.exports = [
             "'self'",
             "https://www.fitraat.com",
             "http://localhost:3000",
-            "https://detox-frontend-web.vercel.app/",
+            "https://detox-frontend-web.vercel.app",
           ],
         },
       },
@@ -35,7 +35,7 @@ module.exports = [
       origin: [
         "https://www.fitraat.com", // ✅ production frontend
         "http://localhost:3000", // ✅ local dev
-        "https://detox-frontend-web.vercel.app/",
+        "https://detox-frontend-web.vercel.app",
       ],
       headers: ["*"],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
